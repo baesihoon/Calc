6bd8ae7e33ad93d83c80a7a11b7db518e45b0315
basesihoon <1004genius@naver.com>
2017-05-12 23:53:26 +0900

    aa

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -12,9 +12,7 @@ public static void main( String arg[] )
    System.out.print( "Enter second integer: ");
    num2 = input.nextInt();
  
-   System.out.printf( "µ¡¼À: %d" , num1+num2);
-   System.out.printf( "»¬¼À : %d" , num1-num2 );
-   System.out.printf( "°ö¼À : %d" , num1*num2 );
+
 
    }
 }
